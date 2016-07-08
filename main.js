8eabcc63e8d71a365c40e37b99f46645814b5dcb
Eamon White <eamon.white7@gmail.com>
2016-07-08 18:15:57 -0400

    links working for projects

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         //Navigation
         menu: '#menu',
         lockAnchors: false,
-        anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage'],
+        anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
         navigation: false,
         navigationPosition: 'right',
         navigationTooltips: ['firstSlide', 'secondSlide', 'thirdSlide'],
